c92abfc57be119aaa3724709ff6d1b6be808b6d0
jessicamcg <jessicamcguico@gmail.com>
2021-11-18 10:07:29 -0600

    setting up service worker

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+  
+const PRECACHE = 'precache-budget';
+const RUNTIME = 'runtime-budget';
+  
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+});
+  
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(RUNTIME).then((cache) => {
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            });
+          });
+        })
+      );
+    }
+});
+  
